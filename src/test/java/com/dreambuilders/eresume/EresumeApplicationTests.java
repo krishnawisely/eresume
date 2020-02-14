ae456413513450c1cd49ed89df80526afcdc119d
krishna <krishna192168@gmail.com>
2020-02-15 00:32:06 +0530

    Added junit

--- src/test/java/com/dreambuilders/eresume/EresumeApplicationTests.java
+++ src/test/java/com/dreambuilders/eresume/EresumeApplicationTests.java
@@ -2,8 +2,9 @@ package com.dreambuilders.eresume;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-@SpringBootTest
+@SpringJUnitConfig
 class EresumeApplicationTests {
 
 	@Test
